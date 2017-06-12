96d2b5b75229dd00d5b44fc1def5d23f4399ca5d
gareththegeek <gj_williams2000@yahoo.co.uk>
2017-06-12 16:22:26 +0100

    Write train times to panel

--- public/app.js
+++ public/app.js
@@ -1,5 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-    
+
+    var trainPanels = _(document.querySelectorAll(".train-times"));
+
+    trainPanels.forEach(function (div) {
+        div.innerHTML = '<dtitle>Train Times<hr /></dtitle><div>Loading</div>';
+    });
+
     apis.transport.findStations('bath spa')
         .then(function (response) {
 
@@ -8,7 +14,31 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
             apis.transport.live(station.station_code)
                 .then(function (response) {
-                    console.log(response);
+
+                    trainPanels.forEach(function (div) {
+
+                        var html = ' \
+                            <dtitle>Train Times</dtitle> \
+                            <hr /> \
+                            <ul>';
+
+                        _(response).forEach(function (item) {
+
+                            var itemClass = item.status === 'LATE' ? 'bad' : 'good';
+
+                            html += '<li class="' + itemClass + '">' + item.expected;
+                            if (item.expected !== item.due) {
+                                html += ' (' + item.due + ')';
+                            }
+                            html += ' ' + item.to + ' ' + item.status + '</li>';
+                        });
+
+
+                        html += '</ul>';
+
+                        div.innerHTML = html;
+                    });
+
                 });
         });
 });
