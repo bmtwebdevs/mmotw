bd4c6e5e47a47aca8a6700beb5cd793c4855203a
gareththegeek <gj_williams2000@yahoo.co.uk>
2017-06-12 12:51:52 +0100

    Create public folder

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 http.listen(3000, function(){
