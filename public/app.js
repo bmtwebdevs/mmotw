44edd4de931953f4fe98c99a260e6ebe19899f8b
gareththegeek <gj_williams2000@yahoo.co.uk>
2017-06-13 12:42:36 +0100

    Added voice recognition

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
-document.addEventListener("DOMContentLoaded", function (event) {
 
-    var trainPanels = _(document.querySelectorAll(".train-times"));
+
+function traintimes() {
+    var trainPanels = document.querySelectorAll(".train-times");
 
     trainPanels.forEach(function (div) {
         div.innerHTML = '<dtitle>Train Times<hr /></dtitle><div>Loading</div>';
@@ -22,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
                             <hr /> \
                             <ul>';
 
-                        _(response).forEach(function (item) {
+                        response.forEach(function (item) {
 
                             var itemClass = item.status === 'LATE' ? 'bad' : 'good';
 
@@ -41,4 +42,58 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
                 });
         });
+}
+
+var hideTime;
+
+function voiceReceived(event) {
+    console.log('You said: ', event.results[0][0].transcript);
+
+    hideTime = new Date();
+    hideTime.setMilliseconds(hideTime.getMilliseconds() + 2990);
+
+    var speechFeedback = document.querySelectorAll('.speech-feedback')[0];
+    speechFeedback.classList.remove("hidden");
+
+    Typed.new(".speech-feedback span", {
+        strings: [event.results[0][0].transcript],
+        typeSpeed: 0
+    });
+
+    setTimeout(function () {
+
+        var now = new Date();
+        if (now < hideTime) {
+            return;
+        }
+        speechFeedback.classList.add("hidden");
+    }, 3000);
+}
+
+function voice() {
+
+    var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+    recognition.lang = 'en-GB';
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 5;
+    recognition.start();
+
+    recognition.onresult = voiceReceived;
+
+    recognition.onend = function (event) {
+        recognition.start();
+    };
+}
+
+function triggerVoice(message) {
+    voiceReceived({ results: [[{ transcript: message }]] });
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    voice();
+    traintimes();
+
+    setTimeout(function () { triggerVoice("Hello World") }, 1000);
+    //setTimeout(function () { triggerVoice("Hello Gareth") }, 3900);
 });
