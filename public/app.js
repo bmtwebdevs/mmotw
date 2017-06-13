f3f522d4d3ae9e8a807d12bbf22e5eb88156f198
moshina84 <moshin.alyas@googlemail.com>
2017-06-13 12:37:50 +0100

    Updated Date format

--- public/app.js
+++ public/app.js
@@ -5,11 +5,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
         console.log(response);
         
         //Convert EPOCH date to human readable
-        var utcSeconds = response.dt;
-        var dateTime = new Date(0); 
-        dateTime.setUTCSeconds(utcSeconds);
-               
-        
+        var timestamp = response.dt;
+        var dateTime = new Date(timestamp*1000);        
+        var year = dateTime.getFullYear();
+        var month = dateTime.getMonth() + 1;
+        var day = dateTime.getDate();                
+        var dateTime1 = day + '-' + month + '-' + year;                
+                       
         var weatherPanel = _(document.querySelectorAll(".weather-data"));
 
         weatherPanel.forEach(function (div) {
@@ -24,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
             
             html += '<font size=5>';
 
-            html += '<div>' + dateTime + '</div><br>';
+            html += '<div>' + dateTime1 + '</div><br>';
             
             html += '<div>' + response.weather[0].main + '</div>';
             
