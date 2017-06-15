d8f04f1a92b416c7e6df86175a2c140084607d71
Yolanta Beresna <yolanta.beres@gmail.com>
2017-06-15 13:14:49 +0100

    Updated calendar and tasks components to use access token and created new page called authenticate. Deleted old ones.

---
+++ public/panels/calendar.js
@@ -0,0 +1,62 @@
+var calendarPanel = calendarPanel || (function () {
+
+    var panel;
+
+
+    function update() {
+
+      const html = '<h4>Calendar</h4> \
+      <div class="timetable"></div>';
+
+      const today = new Date(); var start = new Date(); var end = new Date();
+      start = new Date(start.setHours(1));
+      start = start.setDate(today.getDate() - (today.getDay()));
+      start = (new Date(start)).toISOString();
+
+      end = new Date(end.setHours(23));
+      end = end.setDate(today.getDate() + (6 - today.getDay()));
+      end = (new Date(end)).toISOString();
+
+      apis.calendar.getCalendar(start, end)
+          .then(function (response) {
+            //console.log(response);
+            const events = response.items;
+            if (events.length > 0) {
+              const timetable = new Timetable();
+              timetable.setScope(9, 17);
+              const days =['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+              timetable.addLocations(days);
+              for (i = 0; i < events.length; i++) {
+                const event = events[i];
+                const whenStart = event.start.dateTime;
+                const whenEnd = event.end.dateTime;
+                if (!whenStart) {
+                  whenStart = event.start.date;
+                }
+                if (!whenEnd) {
+                  whenEnd = event.end.date;
+                }
+                var dayIndex = new Date(whenStart).getDay();
+                timetable.addEvent(event.summary, days[dayIndex], new Date(whenStart), new Date(whenEnd));
+              }
+              var renderer = new Timetable.Renderer(timetable);
+              renderer.draw('.timetable');
+            } else {
+              html += '<p> No upcoming events found. </p>';
+            }
+          });
+            panel.innerHTML = html;
+    }
+
+    function attach(p) {
+
+        panel = p;
+
+        update();
+    }
+
+    return {
+        attach: attach
+    };
+
+})();
--- public/panels/tasks.js
+++ public/panels/tasks.js
@@ -2,16 +2,38 @@ var tasks = tasks || (function () {
 
     var panel;
 
-
+    function appendPre(message) {
+      var pre = document.getElementById('tasks-content');
+      var textContent = document.createTextNode(message + '\n');
+      pre.appendChild(textContent);
+    }
 
     function update() {
-
       panel.innerHTML = '<h4>Tasks</h4> \
       <pre id="tasks-content"></pre>';
+      const today = new Date(); var start = new Date(); var end = new Date();
+      start = new Date(start.setHours(1));
+      start = start.setDate(today.getDate() - (today.getDay()));
+      start = (new Date(start)).toISOString();
 
-      //panel.innerHTML = '<h4>Calendar</h4> \<pre id="content"></pre>';
+      end = new Date(end.setHours(23));
+      end = end.setDate(today.getDate() + (6 - today.getDay()));
+      end = (new Date(end)).toISOString();
 
-
+      apis.tasks.getTasks(start, end)
+          .then(function (response) {
+            var taskLists = response.items;
+            if (taskLists && taskLists.length > 0) {
+              for (var i = 0; i < taskLists.length; i++) {
+                var taskList = taskLists[i];
+                console.log(taskList);
+                const days =['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+                if (taskList.title != "") appendPre(taskList.title +" DUE: " + days[new Date(taskList.due).getDay()]);
+              }
+            } else {
+              appendPre('No task lists found.');
+            }
+      });
     }
 
     function attach(p) {
