d80782d561a3090015e816c62a6f35bb43fd1561
dougajmcdonald <dougajmcdonald@gmail.com>
2017-06-15 10:51:02 +0100

    shoddy callback reverse geolocation

--- public/apis/positionapi.js
+++ public/apis/positionapi.js
@@ -1,20 +1,65 @@
 var apis = apis || {};
 apis.position = apis.position || (function () {
 
+
     function getCurrentPosition(callback) {
         if ("geolocation" in navigator) {
           /* geolocation is available */
-          navigator.geolocation.getCurrentPosition(function(position) { callback(position) });
+          navigator.geolocation.getCurrentPosition(function(position) {
+              callback(position)
+          });
         } else {
           console.log('GeoLocation not available');
         }
     }
 
-    function getCurrentLocation() {
-        // TODO: find a town from a lat/lon
+    function getCurrentLocation(callback) {
+
+        if (navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(function(position) {
+
+
+            // TODO: find a town from a lat/lon
+            var geocoder;
+            geocoder = new google.maps.Geocoder();
+            var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+                return geocoder.geocode({'latLng': latlng}, function(results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            if (results[0]) {
+                                var add= results[0].formatted_address ;
+                                var value=add.split(",");
+
+                                count=value.length;
+
+                                callback({
+                                    country :value[count-1],
+                                    state: value[count-2],
+                                    city: value[count-3]
+                                });
+
+                            }
+                            else  {
+                                callback({
+                                    city: "address not found"
+                                });
+                            }
+                        }
+                        else {
+                            callback({ error: "Geocoder failed due to: " + status });
+                        }
+                    });
+                });
+        }
+        else{
+            x.innerHTML="Geolocation is not supported by this browser.";
+        }
+
+
     }
 
     return {
-        getCurrentPosition: getCurrentPosition
+        getCurrentPosition: getCurrentPosition,
+        getCurrentLocation: getCurrentLocation
     };
 })();
--- public/app.js
+++ public/app.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", function (event) {
     layout.addRow();
     layout.addPanel(userprofile);
     layout.addPanel(blank);
-    layout.addPanel(time, { height: '200px', 'min-height': '200px' });
+    layout.addPanel(time, { height: '200px' });
 
     layout.addRow();
-    layout.addPanel(weather, { height: '200px', 'min-height': '200px' });
+    layout.addPanel(weather, { height: '200px' });
     layout.addPanel(blank);
     layout.addPanel(travel);
 
